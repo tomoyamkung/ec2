3b5747ac361f18a6dc4e4ad136a2d62c7d1fb8ce
tomoyamkung <tsuyoshi.sugiyama@gmail.com>
2022-08-13 17:25:53 +0900

    :fire:不要なファイルを削除

--- tests/test_lambda_function.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src.lambda_function import echo
-
-
-@pytest.mark.parametrize(("param", "expected"), [("hoge", "hoge")])
-def test_echo(param, expected):
-    assert echo(param) == expected
